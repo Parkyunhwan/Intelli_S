379f86f069c41a62c337393d70a10907cf669db3
Yunhwan Park <pyh8618@gmail.com>
2020-11-01 14:05:15 +0900

    Save AndroidX version

--- app/src/main/java/com/login_signup_screendesign_demo/ContentActivity.java
+++ app/src/main/java/com/login_signup_screendesign_demo/ContentActivity.java
@@ -2,7 +2,7 @@ package com.login_signup_screendesign_demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.login_signup_screendesign_demo.api.IntelliSApi;
--- app/src/main/java/com/login_signup_screendesign_demo/list/ReservationActivity.java
+++ app/src/main/java/com/login_signup_screendesign_demo/list/ReservationActivity.java
@@ -5,7 +5,7 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
